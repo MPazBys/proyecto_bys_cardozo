72ae1a89e7d2f1e1f874f8101918a552346fa5e9
cmica05 <micaelacardozo3794@gmail.com>
2025-09-14 22:38:47 -0300

    Add files via upload

---
+++ CapaPresentacion/frmCategoria.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BGuardar = new FontAwesome.Sharp.IconButton();
+            this.BEditar = new FontAwesome.Sharp.IconButton();
+            this.BEliminar = new FontAwesome.Sharp.IconButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSeleccionar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.ID_Categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 358);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Categoria:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.BackColor = System.Drawing.Color.SeaGreen;
+            this.BGuardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BGuardar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BGuardar.ForeColor = System.Drawing.Color.White;
+            this.BGuardar.IconChar = FontAwesome.Sharp.IconChar.WindowRestore;
+            this.BGuardar.IconColor = System.Drawing.Color.White;
+            this.BGuardar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BGuardar.IconSize = 16;
+            this.BGuardar.Location = new System.Drawing.Point(29, 219);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(166, 26);
+            this.BGuardar.TabIndex = 3;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BGuardar.UseVisualStyleBackColor = false;
+            // 
+            // BEditar
+            // 
+            this.BEditar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BEditar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BEditar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BEditar.ForeColor = System.Drawing.Color.White;
+            this.BEditar.IconChar = FontAwesome.Sharp.IconChar.Edit;
+            this.BEditar.IconColor = System.Drawing.Color.White;
+            this.BEditar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BEditar.IconSize = 16;
+            this.BEditar.Location = new System.Drawing.Point(29, 256);
+            this.BEditar.Name = "BEditar";
+            this.BEditar.Size = new System.Drawing.Size(166, 23);
+            this.BEditar.TabIndex = 4;
+            this.BEditar.Text = "Editar";
+            this.BEditar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BEditar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BEditar.UseVisualStyleBackColor = false;
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.BackColor = System.Drawing.Color.Maroon;
+            this.BEliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BEliminar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BEliminar.ForeColor = System.Drawing.Color.White;
+            this.BEliminar.IconChar = FontAwesome.Sharp.IconChar.TrashAlt;
+            this.BEliminar.IconColor = System.Drawing.Color.White;
+            this.BEliminar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BEliminar.IconSize = 16;
+            this.BEliminar.Location = new System.Drawing.Point(29, 285);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(166, 23);
+            this.BEliminar.TabIndex = 5;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BEliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BEliminar.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Detalle Categoria";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.btnSeleccionar,
+            this.ID_Categoria,
+            this.Descripcion});
+            this.dataGridView1.Location = new System.Drawing.Point(232, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(357, 246);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(229, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(360, 33);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Lista de Categorias:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.HeaderText = "";
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            // 
+            // ID_Categoria
+            // 
+            this.ID_Categoria.HeaderText = "ID_Categoria";
+            this.ID_Categoria.Name = "ID_Categoria";
+            this.ID_Categoria.Visible = false;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.Width = 150;
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(164, 55);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(31, 20);
+            this.textId.TabIndex = 9;
+            this.textId.Visible = false;
+            // 
+            // frmCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(601, 358);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BEditar);
+            this.Controls.Add(this.BGuardar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCategoria";
+            this.Text = "frmCategoria";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private FontAwesome.Sharp.IconButton BGuardar;
+        private FontAwesome.Sharp.IconButton BEditar;
+        private FontAwesome.Sharp.IconButton BEliminar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewButtonColumn btnSeleccionar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID_Categoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.TextBox textId;
+    }
+}
